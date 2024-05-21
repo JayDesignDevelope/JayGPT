6516060ee3cf5a3e3c22cbd5efb039060d44a0ce
JayDesignDevelope <jay.design.developer@gmail.com>
2024-05-21 20:38:02 +0530

    added footer hide

--- app.py
+++ app.py
@@ -79,3 +79,10 @@ if prompt1:
 
 
 
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
